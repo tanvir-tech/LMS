7cc85f285e5bcd057dfbe02d44395e78c8a19fa1
tanvir-tech <tanvirah325@gmail.com>
2023-02-04 00:03:39 +0600

    categories home page

--- resources/views/lms/home.blade.php
+++ resources/views/lms/home.blade.php
@@ -5,10 +5,150 @@
 
         {{-- {{View::make('auth/login')}} --}}
 
-        <h1>
-            Home page
-        </h1>
 
 
+        <section style="background-color: rgba(238, 238, 238, 0.664);">
+            <div class="container p-5">
+                <h2 class="text-center text-white mb-5"><strong>Categories</strong></h2>
+
+                <div class="row">
+                    <div class="col-lg-4 col-md-12 mb-4">
+                        <div class="card">
+                            <div class="card-body bg-warning">
+
+                            </div>
+                            <div class="card-footer ">
+                                <div class="card-title">
+                                    <h1 class="text-center">
+                                        ICT
+                                    </h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-12 mb-4">
+                        <div class="card">
+                            <div class="card-body bg-warning">
+
+                            </div>
+                            <div class="card-footer ">
+                                <div class="card-title">
+                                    <h1 class="text-center">
+                                        Math
+                                    </h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-12 mb-4">
+                        <div class="card">
+                            <div class="card-body bg-warning">
+
+                            </div>
+                            <div class="card-footer ">
+                                <div class="card-title">
+                                    <h1 class="text-center">
+                                        Physics
+                                    </h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-4 col-md-12 mb-4">
+                        <div class="card">
+                            <div class="card-body bg-warning">
+
+                            </div>
+                            <div class="card-footer ">
+                                <div class="card-title">
+                                    <h1 class="text-center">
+                                        English
+                                    </h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-12 mb-4">
+                        <div class="card">
+                            <div class="card-body bg-warning">
+
+                            </div>
+                            <div class="card-footer ">
+                                <div class="card-title">
+                                    <h1 class="text-center">
+                                        Biology
+                                    </h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-12 mb-4">
+                        <div class="card">
+                            <div class="card-body bg-warning">
+
+                            </div>
+                            <div class="card-footer ">
+                                <div class="card-title">
+                                    <h1 class="text-center">
+                                        Electrical
+                                    </h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-4 col-md-12 mb-4">
+                        <div class="card">
+                            <div class="card-body bg-warning">
+
+                            </div>
+                            <div class="card-footer ">
+                                <div class="card-title">
+                                    <h1 class="text-center">
+                                        Statistics
+                                    </h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-12 mb-4">
+                        <div class="card">
+                            <div class="card-body bg-warning">
+
+                            </div>
+                            <div class="card-footer ">
+                                <div class="card-title">
+                                    <h1 class="text-center">
+                                        Social Science
+                                    </h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-12 mb-4">
+                        <div class="card">
+                            <div class="card-body bg-warning">
+
+                            </div>
+                            <div class="card-footer ">
+                                <div class="card-title">
+                                    <h1 class="text-center">
+                                        Papers
+                                    </h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+
+        </section>
     </div>
 @endsection
