b49467acb348c4c78ff1ff89ac78cda66f081d23
tanvir-tech <tanvirah325@gmail.com>
2023-03-14 12:06:02 +0600

    lms logo

--- resources/views/includes/header2.blade.php
+++ resources/views/includes/header2.blade.php
@@ -1,12 +1,10 @@
 <header id="page-topbar">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light p-2">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light p-3">
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03"
             aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <a class="navbar-brand" href="/home">
-            <img src="{{ asset('gallery')}}/lms-logo.png" alt="lms logo" width="110px">
-        </a>
+        <a class="navbar-brand" href="/home">LMS</a>
 
         <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
             <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
